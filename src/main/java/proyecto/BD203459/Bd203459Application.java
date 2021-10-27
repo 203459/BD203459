54f1a6c5c6033f5a3e285d4b58a2b62fd86a1c9f
203459 <203459@ids.upchiapas.edu.mx>
2021-10-27 11:36:13 -0500

    primer commit

---
+++ src/main/java/proyecto/BD203459/Bd203459Application.java
@@ -0,0 +1,13 @@
+package proyecto.BD203459;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bd203459Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bd203459Application.class, args);
+	}
+
+}
---
+++ src/test/java/proyecto/BD203459/Bd203459ApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.BD203459;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bd203459ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
